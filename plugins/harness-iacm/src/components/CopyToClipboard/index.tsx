29c527f3fff5ac9eb0099b966140c6d8948403a2
Simon Lyttle <simon.lyttle@harness.io>
2024-12-04 15:51:23 +0000

    feat: [IAC-2850]: IACM Plugin workspace outputs -refactor

--- plugins/harness-iacm/src/components/CopyToClipboard/index.tsx
+++ plugins/harness-iacm/src/components/CopyToClipboard/index.tsx
@@ -1,11 +1,22 @@
 import React, { FC } from 'react';
-import { Button } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 import CopyIcon from '@material-ui/icons/FileCopyOutlined';
 
 interface Props {
   copyValue?: string;
 }
+
+const useStyles = makeStyles(theme => ({
+  iconButton: {
+    color: '#0A6EBE',
+    width: 15,
+    height: 15,
+  },
+}));
+
 const CopyToClipboard: FC<Props> = ({ copyValue }) => {
+  const classes = useStyles();
+
   const copy = (content?: string) => async () => {
     try {
       await navigator.clipboard.writeText(content || '');
@@ -15,7 +26,7 @@ const CopyToClipboard: FC<Props> = ({ copyValue }) => {
   };
   return (
     <Button onClick={copy(copyValue)} variant="text">
-      <CopyIcon style={{ color: '#0A6EBE', width: '15', height: '15' }} />
+      <CopyIcon className={classes.iconButton} />
     </Button>
   );
 };
--- plugins/harness-iacm/src/components/WorkspaceList/useGetWorkspaceTableColumns.tsx
+++ plugins/harness-iacm/src/components/WorkspaceList/useGetWorkspaceTableColumns.tsx
@@ -1,5 +1,5 @@
 import { TableColumn } from '@backstage/core-components';
-import { Link, Typography } from '@material-ui/core';
+import { Button, Link, Typography, makeStyles } from '@material-ui/core';
 import React, { useMemo } from 'react';
 import { TableData } from '../../types';
 
@@ -9,123 +9,118 @@ import Gcp from '../../icons/gcp';
 import Kubernetes from '../../icons/kubernetes';
 import { Output } from '../../hooks/useGetResources';
 import CopyToClipboard from '../CopyToClipboard';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    width: '100%',
+  },
+  flexCenter: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  smallGreyText: {
+    fontSize: 'small',
+    color: 'grey',
+  },
+  gap: {
+    display: 'flex',
+    gap: theme.spacing(0.5),
+  },
+  iconButton: {
+    color: '#0A6EBE',
+    width: 15,
+    height: 15,
+  },
+  paddingRightSmall: {
+    paddingRight: theme.spacing(0.5),
+  },
+}));
 
 export const useGetWorkspaceTableColumns = ({
   baseUrl,
 }: {
   baseUrl: string;
 }) => {
+  const classes = useStyles();
+
   const getProviderIcon = (name?: string): React.ReactNode => {
     if (!name) return <Aws />;
-    if (name.includes('aws')) {
-      return <Aws />;
-    }
-
-    if (name.includes('azure')) {
-      return <Azure />;
-    }
-    if (name.includes('gcp') || name.includes('google')) {
-      return <Gcp />;
-    }
-    if (name.includes('kubernetes') || name.includes('k8s')) {
+    if (name.includes('aws')) return <Aws />;
+    if (name.includes('azure')) return <Azure />;
+    if (name.includes('gcp') || name.includes('google')) return <Gcp />;
+    if (name.includes('kubernetes') || name.includes('k8s'))
       return <Kubernetes />;
-    }
     return null;
   };
+
   const resourceColumns: TableColumn[] = useMemo(
     () => [
       {
         title: 'Provider',
         field: 'col1',
         width: '22%',
-        render: (row: Partial<TableData>) => {
-          return (
-            <Link
-              href={baseUrl}
-              target="_blank"
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-              key={row.id}
-            >
-              <b>{row.provider}</b>
-            </Link>
-          );
-        },
-        customFilterAndSearch: (term, row: Partial<TableData>) => {
-          const temp = row?.provider ?? '';
-          return temp.toLowerCase().indexOf(term.toLowerCase()) > -1;
-        },
-        customSort: (row1: Partial<TableData>, row2: Partial<TableData>) => {
-          const a = row1.provider ?? '';
-          const b = row2.provider ?? '';
-          return a > b ? 1 : -1;
-        },
+        render: (row: Partial<TableData>) => (
+          <Link
+            href={baseUrl}
+            target="_blank"
+            className={classes.flexCenter}
+            key={row.id}
+          >
+            <b>{row.provider}</b>
+          </Link>
+        ),
+        customFilterAndSearch: (term, row: Partial<TableData>) =>
+          (row.provider ?? '').toLowerCase().includes(term.toLowerCase()),
+        customSort: (row1: Partial<TableData>, row2: Partial<TableData>) =>
+          (row1.provider ?? '').localeCompare(row2.provider ?? ''),
       },
       {
         title: 'Type',
         field: 'col2',
         width: '18%',
         render: (row: Partial<TableData>) => (
-          <span style={{ display: 'flex', gap: '5px' }}>
+          <span className={classes.gap}>
             {getProviderIcon(row.provider)}
-            <Typography style={{ fontSize: 'small', color: 'grey' }}>
+            <Typography className={classes.smallGreyText}>
               {row.type}
             </Typography>
           </span>
         ),
-        customFilterAndSearch: (term, row: Partial<TableData>) => {
-          const temp = row?.type ?? '';
-          return temp.toLowerCase().indexOf(term.toLowerCase()) > -1;
-        },
-        customSort: (row1: Partial<TableData>, row2: Partial<TableData>) => {
-          const a = row1.type ?? '';
-          const b = row2.type ?? '';
-          return a > b ? 1 : -1;
-        },
+        customFilterAndSearch: (term, row: Partial<TableData>) =>
+          (row.type ?? '').toLowerCase().includes(term.toLowerCase()),
+        customSort: (row1: Partial<TableData>, row2: Partial<TableData>) =>
+          (row1.type ?? '').localeCompare(row2.type ?? ''),
       },
       {
         title: 'Name',
         field: 'col3',
         width: '30%',
         render: (row: Partial<TableData>) => (
-          <Typography style={{ fontSize: 'small', color: 'grey' }}>
-            {row.name}
-          </Typography>
+          <Typography className={classes.smallGreyText}>{row.name}</Typography>
         ),
-        customFilterAndSearch: (term, row: Partial<TableData>) => {
-          const temp = row?.name ?? '';
-          return temp.toLowerCase().indexOf(term.toLowerCase()) > -1;
-        },
-        customSort: (row1: Partial<TableData>, row2: Partial<TableData>) => {
-          const a = row1.name ?? '';
-          const b = row2.name ?? '';
-          return a > b ? 1 : -1;
-        },
+        customFilterAndSearch: (term, row: Partial<TableData>) =>
+          (row.name ?? '').toLowerCase().includes(term.toLowerCase()),
+        customSort: (row1: Partial<TableData>, row2: Partial<TableData>) =>
+          (row1.name ?? '').localeCompare(row2.name ?? ''),
       },
       {
         title: 'Module',
         field: 'col4',
         width: '20%',
         render: (row: Partial<TableData>) => (
-          <Typography style={{ fontSize: 'small', color: 'grey' }}>
+          <Typography className={classes.smallGreyText}>
             {row.module}
           </Typography>
         ),
-        customFilterAndSearch: (term, row: Partial<TableData>) => {
-          const temp = row?.module ?? '';
-          return temp.toLowerCase().indexOf(term.toLowerCase()) > -1;
-        },
-        customSort: (row1: Partial<TableData>, row2: Partial<TableData>) => {
-          const a = row1.module ?? '';
-          const b = row2.module ?? '';
-          return a > b ? 1 : -1;
-        },
+        customFilterAndSearch: (term, row: Partial<TableData>) =>
+          (row.module ?? '').toLowerCase().includes(term.toLowerCase()),
+        customSort: (row1: Partial<TableData>, row2: Partial<TableData>) =>
+          (row1.module ?? '').localeCompare(row2.module ?? ''),
       },
     ],
-    [baseUrl],
+    [baseUrl, classes],
   );
 
   const outputsColumns: TableColumn[] = useMemo(
@@ -135,50 +130,54 @@ export const useGetWorkspaceTableColumns = ({
         field: 'col3',
         width: '30%',
         render: (row: Partial<Output>) => (
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div className={classes.flexCenter}>
             <Typography
-              style={{ fontSize: 'small', color: 'grey', paddingRight: '5px' }}
+              className={`${classes.smallGreyText} ${classes.paddingRightSmall}`}
             >
               {row.name}
             </Typography>
             <CopyToClipboard copyValue={row.name} />
           </div>
         ),
-        customFilterAndSearch: (term, row: Partial<TableData>) => {
-          const temp = row?.name ?? '';
-          return temp.toLowerCase().indexOf(term.toLowerCase()) > -1;
-        },
-        customSort: (row1: Partial<TableData>, row2: Partial<TableData>) => {
-          const a = row1.name ?? '';
-          const b = row2.name ?? '';
-          return a > b ? 1 : -1;
-        },
+        customFilterAndSearch: (term, row: Partial<Output>) =>
+          (row.name ?? '').toLowerCase().includes(term.toLowerCase()),
+        customSort: (row1: Partial<Output>, row2: Partial<Output>) =>
+          (row1.name ?? '').localeCompare(row2.name ?? ''),
       },
       {
         title: 'Value',
         field: 'col2',
         width: '70%',
-        render: (row: Partial<Output>) => (
-          <span style={{ display: 'flex', gap: '5px', alignItems: 'center' }}>
-            <Typography style={{ fontSize: 'small', color: 'grey' }}>
-              {row.value}
-            </Typography>
-            <CopyToClipboard copyValue={row.value} />
-          </span>
-        ),
-        customFilterAndSearch: (term, row: Partial<Output>) => {
-          const temp = row?.value ?? '';
-          return temp.toLowerCase().indexOf(term.toLowerCase()) > -1;
+        render: (row: Partial<Output>) => {
+          const isSensitive = row.sensitive;
+          const [show, setShow] = React.useState(!isSensitive);
+          return (
+            <span className={classes.flexCenter}>
+              <Typography className={classes.smallGreyText}>
+                {show ? row.value : '********'}
+              </Typography>
+              {show && <CopyToClipboard copyValue={row.value} />}
+              {isSensitive && (
+                <Button onClick={() => setShow(!show)}>
+                  {show ? (
+                    <VisibilityOffIcon className={classes.iconButton} />
+                  ) : (
+                    <VisibilityIcon className={classes.iconButton} />
+                  )}
+                </Button>
+              )}
+            </span>
+          );
         },
-        customSort: (row1: Partial<Output>, row2: Partial<Output>) => {
-          const a = row1.value ?? '';
-          const b = row2.value ?? '';
-          return a > b ? 1 : -1;
-        },
+        customFilterAndSearch: (term, row: Partial<Output>) =>
+          (row.value ?? '').toLowerCase().includes(term.toLowerCase()),
+        customSort: (row1: Partial<Output>, row2: Partial<Output>) =>
+          (row1.value ?? '').localeCompare(row2.value ?? ''),
       },
     ],
-    [],
+    [classes],
   );
+
   return {
     resourceColumns,
     outputsColumns,
